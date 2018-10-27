9406ca50c867beef7ba7fead683cb02dec175da9
Louis Nwamadi <louisdante9@gmail.com>
2018-10-27 23:42:43 +0100

    remove build folder

--- my-app/src/config/fire.js
+++ my-app/src/config/fire.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app';
+import 'firebase/auth';
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB29UTgUadXdVXcakdZ0LimL06HdZdqapE",
