80107194d6d2baab8371f0c2ec52c7bea6177a6d
Louis Nwamadi <louisdante9@gmail.com>
2018-10-27 23:43:32 +0100

    Updates

---
+++ precache-manifest.26ba4a548d52ef7bc7adb15b7b6683ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firebase_react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "358c1c1272b078aedbe1",
+    "url": "/firebase_react/static/js/runtime~main.358c1c12.js"
+  },
+  {
+    "revision": "276d09bdce9fc4407f09",
+    "url": "/firebase_react/static/js/main.276d09bd.chunk.js"
+  },
+  {
+    "revision": "819a2ab3e45dec38497d",
+    "url": "/firebase_react/static/js/1.819a2ab3.chunk.js"
+  },
+  {
+    "revision": "276d09bdce9fc4407f09",
+    "url": "/firebase_react/static/css/main.c9176b57.chunk.css"
+  },
+  {
+    "revision": "c070166668e6be974fcbad427ec2229d",
+    "url": "/firebase_react/index.html"
+  }
+];
